b6ffb1a378f43997db9aaa42e2f441b01e6d9be9
kiransingh99 <kiran.jolly@gmail.com>
2021-02-18 15:55:13 +0000

    restructure raw_data to separate health+individual

--- src/13c-post_processing/obj/raw_data.py
+++ src/13c-post_processing/obj/raw_data.py
@@ -1,45 +1,52 @@
 from . import functions
 from . import global_tracker
+from . import constants
 import os.path
 import csv
 
 class RawData:
     #handles everything to do with raw data files
+    def __init__(self, 
+                    showWarnings=True, number_of_columns=7,
+                    fileName=""):
+        self.showWarnings = showWarnings
+        self.number_of_columns = number_of_columns
+        self.fileName = fileName
 
-    def __init__(self,
-                    showWarnings = True,
-                    rawDataDirectory="..\\..\\..\\data\\",
-                    number_of_columns=7):
+    @property
+    def health(self):
+        return RawDataHealthChecker(self.showWarnings, self.number_of_columns)
+    
+    @property
+    def individual(self):
+        return SingleRawDataFile(self.fileName)
 
-        currentPath = os.path.abspath(os.path.dirname(__file__))
 
+class RawDataHealthChecker:
+    def __init__(self, showWarnings, number_of_columns):
+        
         #set class constants
-        self.__RAW_DATA_DIRECTORY = os.path.join(currentPath, rawDataDirectory)
         self.__NUMBER_OF_COLUMNS = 7
-        self.__MAX_THROW_TIME = 30000
+        self.__THROW_TIME_WARNING_THRESHOLD = 25000 
         self.__ACCELEROMETER_WARNING_THRESHOLD = 150 #accelerometer set to 16G
         self.__GYRO_WARNING_THRESHOLD = 1900 #gyro set to 2000dps
         
         #define class variables
-        self.showWarnings = True
+        self.showWarnings = showWarnings
         self.__warningsRaised = False
 
-        print("raw data initialised at", self.RAW_DATA_DIRECTORY, "\n")
+        print("Raw data initialised at", constants.RAW_DATA_DIRECTORY, "\n")
 
     def __del__(self):
         print("RawData object destroyed")
-
-    @property
-    def RAW_DATA_DIRECTORY(self):
-        return self.__RAW_DATA_DIRECTORY
 
     @property
     def NUMBER_OF_COLUMNS(self):
         return self.__NUMBER_OF_COLUMNS
 
     @property
-    def MAX_THROW_TIME(self):
-        return self.__MAX_THROW_TIME
+    def THROW_TIME_WARNING_THRESHOLD(self):
+        return self.__THROW_TIME_WARNING_THRESHOLD
 
     @property
     def ACCELEROMETER_WARNING_THRESHOLD(self):
@@ -50,7 +57,7 @@ class RawData:
         return self.__GYRO_WARNING_THRESHOLD
 
 
-    def check_health_all_files(self):
+    def check_all_files(self):
         #iterate through each file in the folder and call check_health_one_file
         
         files_total = 0
@@ -58,15 +65,15 @@ class RawData:
         files_failed = []
         tests_failed = []
 
-        for entry in os.listdir(self.RAW_DATA_DIRECTORY):
-            filePath = os.path.join(self.RAW_DATA_DIRECTORY, entry)
+        for entry in os.listdir(constants.RAW_DATA_DIRECTORY):
+            filePath = os.path.join(constants.RAW_DATA_DIRECTORY, entry)
             if entry.endswith(".csv"): 
                 files_total += 1
-                allTestsPassed, error = self.check_health_one_file(filePath)
+                allTestsPassed, error = self.check_one_file(filePath)
                 if allTestsPassed:
                     files_passed += 1
                 else:
-                    files_failed.append(filePath[len(self.RAW_DATA_DIRECTORY)-6:])
+                    files_failed.append(filePath[len(constants.RAW_DATA_DIRECTORY)-6:])
                     tests_failed.append(error)
         
         print("\n\nAll tests complete. {}/{} passed.".format(files_passed, 
@@ -77,20 +84,19 @@ class RawData:
                 print("  {} : {}\n".format(files_failed[i], error[i]))
         return(1)
 
-    def check_health_one_file(self, filePath):
+    def check_one_file(self, filePath):
         #call other functions to check file health
         #if any tests return false then end checks and return False
         #only exception is if times can be corrected then return true, but make 
         #it clear that it should be checked
         
-        fileName = filePath[len(self.RAW_DATA_DIRECTORY)-6:]
-
-        self.__add_file_to_global(fileName)
+        fileName = filePath[len(constants.RAW_DATA_DIRECTORY)-6:]
 
         with open(filePath) as f:
             csv_file = csv.reader(f, delimiter=',')
             print("Testing {}... ".format(fileName), end="")
 
+            errorFree = True
             previousTime = 0
 
             for row in csv_file:
@@ -101,7 +107,11 @@ class RawData:
                 except AssertionError as e:
                     print("Test failed:")
                     print("  {}\n".format(e.args[0]))
+                    errorFree = False
+
                     return(0, e.args[0].split(":"))
+
+        self.__add_file_to_tracker(fileName, errorFree)
 
         if self.__warningsRaised:
             print("\nAll tests passed\n") #adjust formatting if warnings have been raised
@@ -110,7 +120,6 @@ class RawData:
             print("All tests passed")
 
         return(1, None)
-
 
     def __check_columns(self, row, line_num):
         #check that each row has exactly 7 columns
@@ -131,9 +140,9 @@ class RawData:
                 value = float(value_str)
 
                 if i == 0:
-                    if int(value) > self.MAX_THROW_TIME: #if time gets near 30 milliseconds
+                    if int(value) > self.THROW_TIME_WARNING_THRESHOLD: #if time gets near 30 milliseconds
                         print("\nWarning: Time of throw exceeds {} on line {} ( = {})"
-                                .format(self.MAX_THROW_TIME, line_num, value),
+                                .format(self.THROW_TIME_WARNING_THRESHOLD, line_num, value),
                                 end="")
                         self.__warningsRaised = True
                 elif i <= 3:
@@ -160,19 +169,31 @@ class RawData:
                 .format(row, row[0], previousTime)
         return int(row[0])
 
-    def __add_file_to_global(self, fileName):
+    def __add_file_to_tracker(self, fileName, errorFree):
         #sends the names of the files to the GlobalFile class to get added
         G = global_tracker.GlobalFile()
-        G.add_file(fileName)
+        G.add_file(fileName, errorFree)
         del G
 
-    def add_metrics_to_global(self, metric):
+
+class SingleRawDataFile:
+    def __init__(self, fileName):
+        pass
+
+    
+    def __add_file_to_tracker(self, fileName, errorFree):
+        #sends the names of the files to the GlobalFile class to get added
+        G = global_tracker.GlobalFile()
+        G.add_file(fileName, errorFree)
+        del G
+
+    def add_metrics_to_tracker(self, metric):
         #sends the relevant files and data to GlobalFile class to get added
         #metric is the name of the metric (string)
         #call populate_metric
         pass
 
-    def __writeToTracker(self, file, metric, data):
+    def __write_to_tracker(self, file, metric, data):
         #iterate through all the raw files, and calculate metric, and then 
         #write it to global tracker file under the appropriate metric heading
         pass
