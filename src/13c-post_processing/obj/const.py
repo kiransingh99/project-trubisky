c46f45d45c5ba0d7cd7da025acd79a7f3c3be465
kiransingh99 <kiran.jolly@gmail.com>
2021-02-19 23:43:06 +0000

    added comments to supporting files

--- src/13c-post_processing/obj/const.py
+++ src/13c-post_processing/obj/const.py
@@ -1,12 +1,16 @@
 import os.path
 
+# private constants used to set up public ones
 __currentPath = os.path.abspath(os.path.dirname(__file__))
 __dataDirectory="..\\..\\..\\data\\"
 
+# absolute path to directory where data files are stored
 DATA_DIRECTORY = os.path.join(__currentPath, __dataDirectory)
+PATH_LENGTH_TO_DATA_DIR = len(DATA_DIRECTORY)-6
 
+# for the global tracker file
 TRACKER_FILENAME = "globalTracker.csv"
-TRACKER_FILEPATH = os.path.join(DATA_DIRECTORY, TRACKER_FILENAME)
+TRACKER_FILEPATH = os.path.join(DATA_DIRECTORY, TRACKER_FILENAME) # full file path
 
-NUMBER_OF_COLUMNS = 7
-DELIMITER = ","
+DELIMITER = "," # used in csv files
+NUMBER_OF_COLUMNS = 7 # number of columns in raw data csv files
--- src/13c-post_processing/obj/functions.py
+++ src/13c-post_processing/obj/functions.py
@@ -1,4 +1,13 @@
 def isFloat(value):
+  """Checks if a string can be converted into a floating point value.
+
+  Args:
+      value (str): test if this object can be converted into a float
+
+  Returns:
+      bool: true if 'value' can be converted into a float, false otherwise
+  """
+  
   try:
     float(value)
     return True
