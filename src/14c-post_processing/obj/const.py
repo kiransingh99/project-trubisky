fe051cc8cf6761c6a07524da8e938cc28474107b
kiransingh99 <kiran.jolly@gmail.com>
2021-03-09 03:24:49 +0000

    minor amendments to raw/processed data constants

--- src/14c-post_processing/obj/const.py
+++ src/14c-post_processing/obj/const.py
@@ -16,27 +16,29 @@ TRACKER_FILEPATH = os.path.join(DATA_DIRECTORY, TRACKER_FILENAME) # full file pa
 TRACKER_BARE_MINIMUM = "name,health status,processed file" # bare minimum content of the tracker file (just the header)
 TRACKER_BARE_MINIMUM_LENGTH = len(TRACKER_BARE_MINIMUM.split(","))
 
-# for raw data files
-COLUMN_HEADERS = ["time", "acc_x", "acc_y", "acc_z", "w_x", "w_y", "w_z", 
-                    "euler_x", "euler_y", "euler_z", "mag_x", "mag_y", "mag_z"]
-COLOURS = ['', '#1f77b4', '#ff7f0e', '#26a02c', '#d62728', '#9467bd', '#8c564b',
-            '#e377c2', '#7f7f7f', '#bcbd22', '#17becf', '#00ff00', '#000000']
+# for all data files
+COLUMN_HEADERS = ["time", "acc (e_r)", "acc (e_1)", "acc (e_2)", 
+                            "w (e_r)", "w (e_1)", "w (e_2)", 
+                            "euler (alpha)", "euler (beta)", "euler (gamma)"]
+COLOURS = ['', '#1f77b4', '#ff7f0e', '#26a02c', 
+                '#d62728', '#9467bd', '#8c564b',
+                '#e377c2', '#7f7f7f', '#bcbd22']
 NUMBER_OF_COLUMNS = len(COLUMN_HEADERS) # number of columns in raw data csv files
 
 assert len(COLOURS) == NUMBER_OF_COLUMNS, \
     "Fatal (const.py):There are {} listed column colours; there should be {}."\
         .format(len(COLOURS), NUMBER_OF_COLUMNS)
 
+# for raw data files
 RAW_DATA_PREFIX = "RAW-"
 RAW_DATA_TITLE_FORMAT = RAW_DATA_PREFIX + "yyyy.mm.dd-hh.mm.ss"
 RAW_DATA_FILE_TYPE = ".csv"
 
 # for processed data files
 PROCESSED_DATA_PREFIX = "PRO-"
-PROCESSED_DATA_TITLE_FORMAT = RAW_DATA_PREFIX + "yyyy.mm.dd-hh.mm.ss"
 PROCESSED_DATA_FILE_TYPE = ".csv"
 
-# for health check values
+# health check values
 untested = 0
 failed = 1
 passedWithWarnings = 2
